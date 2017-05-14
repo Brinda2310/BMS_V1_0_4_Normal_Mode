b9fd63c495705259aa50365153dea5b4ab409b48
unknown <nikhil.ingale@asteria.co.in>
2017-05-14 15:53:15 +0530

    Added the files for flash write/erase cycles improvement; tested the logic which is there on STM_Flash_Cycle_Improvement branch and it is working fine

---
+++ Asteria_BMS/Include/BMS_Flash.h
@@ -0,0 +1,27 @@
+/*
+ * BMS_Flash.h
+ *
+ *  Created on: 14-May-2017
+ *      Author: NIKHIL
+ */
+
+#ifndef BMS_FLASH_H_
+#define BMS_FLASH_H_
+
+#include "FLASH_API.h"
+#include "BMS_ISL94203.h"
+
+#define SECTOR_SIZE 				2048
+#define READ_SIZE					8
+#define WRITE_SIZE					READ_SIZE
+
+enum Mem_Write_Results
+{
+	MEM_ADDRESS_INVALID = 0, MEM_DATA_SIZE_OVERLFOW,MEM_DATA_SIZE_INVALID,MEM_WRITE_OK,MEM_READ_OK
+};
+
+uint8_t Write_Data(uint32_t *Memory_Address,uint8_t *TxBuffer,uint8_t Size);
+
+
+
+#endif /* BMS_FLASH_H_ */
