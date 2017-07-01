9441804843f8a2e0934581e32f5154a351d2cd2e
unknown <nikhil.ingale@asteria.co.in>
2017-07-01 20:18:49 +0530

    Added comments to the code and implemented the flight status based sleep mode functionality

--- Asteria_BMS/Source/AP_Communication.c
+++ Asteria_BMS/Source/AP_Communication.c
@@ -13,7 +13,6 @@ uint8_t Send_Byte_Count = 0;
 uint8_t Byte_Count = 1;
 uint8_t Reply_Byte = 0xFF;
 uint8_t SMBUS_Reboot_Count = 0;
-bool Data_Received = false;
 uint8_t AP_Status;
 
 uint8_t AP_Request_Data[MAX_AP_DATA_SIZE];
@@ -63,12 +62,16 @@ void Check_AP_Request()
 
 	if( Result == SMBUS_REQ_SUCCESSFUL)
 	{
-		if(Byte_Count == 1)
+		/* Whenever any request comes from AP, first byte will be always the command code. So state machine
+		 * starts with command code. If there are more number of bytes coming from AP, then state should
+		 * be maintained to it's previous command code e.g. 0x0A is command code for GPS_DATE_TIME. AP will
+		 * send the date and time following the command code. So state should remained to 0x0A to parse
+		 * the data */
+		if(Byte_Count == MINIMUM_PACKET_SIZE)
 		{
 			State = AP_Request_Data[0];
 			Read_Request = true;
 		}
-//		BMS_Debug_COM_Write_Data(&AP_Request_Data[0],Byte_Count);
 
 		switch (State)
 		{
@@ -99,26 +102,31 @@ void Check_AP_Request()
 			case GPS_PACKET_REG:
 				if(Read_Request == true)
 				{
-					Byte_Count = 17;
+					/* Number of bytes to be received from AP GPS packet command code */
+					Byte_Count = GPS_DATE_TIME_DATA_SIZE;
 					Read_Request = false;
 				}
 				else
 				{
-//					BMS_Debug_COM_Write_Data(&AP_Request_Data[0],17);
-					Byte_Count = 1;
+					/* Set the receive byte size to 1 as command code is of one byte only */
+					Byte_Count = MINIMUM_PACKET_SIZE;
 				}
 				Set_Bytes_Count(&Byte_Count);
 				break;
 			case FLIGHT_STATUS_REG:
 				if(Read_Request == true)
 				{
-					Byte_Count = 2;
+					/* Number of bytes to be received from AP FLIGHT_STATUS packet command code */
+					Byte_Count = FLIGHT_STATUS_DATA_SIZE;
 					Read_Request = false;
 				}
 				else
 				{
-//					BMS_Debug_COM_Write_Data(&AP_Request_Data[0],2);
-					Byte_Count = 1;
+#if DEBUG_OPTIONAL == ENABLE
+				BMS_Debug_COM_Write_Data(&AP_Request_Data[0],2);
+#endif
+					AP_Status= AP_Request_Data[0];
+					Byte_Count = MINIMUM_PACKET_SIZE;
 				}
 				Set_Bytes_Count(&Byte_Count);
 				break;
@@ -126,38 +134,56 @@ void Check_AP_Request()
 			default:
 				break;
 		}
+		/* Number of response bytes to be sent to AP. Most of the replies are float so value isset to 4 */
 		if(Read_Request == true)
 		{
 			/* Delay is required to set up the receiving side(AP) */
 			Delay_Millis(4);
 			SMBUS_Serve_Request((uint8_t*)Float_Data, 4);
 		}
+		/* If AP wants to write some data to BMS, then BMS reply with 0xFF byte to AP to indicate that
+		 * it can send the required data(It is write request from AP) */
 		else if (Read_Request == false && Byte_Count != 1)
 		{
 			Delay_Millis(4);
 			SMBUS_Serve_Request((uint8_t*)&Reply_Byte, 1);
 		}
+		SMBUS_Reboot_Count = 0;
 	}
-//	else if(Result == SMBUS_REQ_TIMEOUT)
-//	{
-//		if(AP_COM_Init(AP_COM_SMBUS_MODE) == RESULT_OK)
-//		{
-//			/* This count decides the sleep mode function to be enabled or not. It may be possible that
-//			 * AP has sent the DISARM_GROUND status but later SMBUS stopped working,Just for safety purpose
-//			 * disable sleep mode function */
-//			SMBUS_Reboot_Count++;
-//		}
-//	}
+	else if(Result == SMBUS_REQ_TIMEOUT)
+	{
+		if(AP_COM_Init(AP_COM_SMBUS_MODE) == RESULT_OK)
+		{
+			/* Keep the track of how many times communication is reseted. It will be used to disable the
+			 * sleep mode function as AP is not able to communicate with BMS */
+			SMBUS_Reboot_Count++;
+		}
+	}
+	if (AP_Status == DISARMED_GROUND)
+	{
+		/* Enable the sleep mode functionality only if aircraft is on ground and in disarmed state */
+		Sleep_Mode_Funtionality = ENABLE;
+	}
+	else
+	{
+		Sleep_Mode_Funtionality = DISABLE;
+	}
 	/* Disable sleep mode function of BMS in case SMBUS has restarted 15 times(45 seconds). 45 seconds
 	 * time is because that if load is not present for continuous 1 minute then MCU will go to sleep
-	 * failing to execute the remaining logic */
+	 * failing to execute the remaining logic. If previous request from AP was DISARM and GROUND and for
+	 * some reason AP is not able to communicate with BMS then BMS should not go to sleep mode */
 	if(SMBUS_Reboot_Count >= MAX_SMBUS_REBOOT_COUNT && Sleep_Mode_Funtionality == ENABLE)
 	{
 		Sleep_Mode_Funtionality = DISABLE;
 		/* Debug code to be removed */
+#if DEBUG_OPTIONAL == ENABLE
 		BMS_Debug_COM_Write_Data("Sleep disable\r",14);
 		SMBUS_Reboot_Count = 0;
+#endif
 	}
+	/* There is possibility that sleep mode functionality is enabled due to previous request from AP
+	 * for DISARM and GROUND, So it is necessary to reset the status to zero */
+	AP_Status = 0;
 
 }
 
